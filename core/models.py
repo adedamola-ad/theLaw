8ef623bb27046085f1f6f79e67d4e6e45bb01a12
ifedayo <adesiyanifedayo@gmail.com>
2020-07-18 00:25:14 +0100

    Create Models as SQL Tables

--- core/models.py
+++ core/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+
+#SQL TABLE OF CONTACT
+class Contact(models.Model):
+	name = models.CharField(max_length=40, blank=False, null=False)
+	phone_number = models.IntegerField(null=False, blank=False)
+	email = models.EmailField(max_length=40, null=False, blank=False)
+	message = models.TextField()
+	timestamp = models.DateTimeField(auto_now_add=True)
+
+
+	def __str__(self):
+		return self.name
