a3900b87ab0908936a8029adf12ec5adadbb1081
ifedayo <adesiyanifedayo@gmail.com>
2020-07-18 13:03:45 +0100

    Add forms file and create function for home view

---
+++ core/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from .models import Contact
+
+
+class ContactForm(forms.Form):
+	name = forms.CharField()
+	phone_number = forms.IntegerField()
+	email = forms.CharField()
+	message = forms.TextField()
+
+
+class ContactModelForm(forms.ModelForm):
+    class Meta:
+        model = Contact
+        fields = ['name', 'phone_number', 'email', 'message']
--- core/views.py
+++ core/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def home(request):
+    form = ContactModelForm(request.POST or None, request.FILES or None)
+    if form.is_valid():
+    	obj = form.save(commit=False)
+        obj.save()
+        return redirect('/')
+
+    context = {'form': form}
+    template_name = 'core/home.html'
+    return render(request, template_name, context)
